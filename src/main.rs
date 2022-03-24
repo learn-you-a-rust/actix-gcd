b7b2357370552be6b6d07bdd42fc20d50ab4af7d
greyspectrum <hello@greyspectrum.io>
2022-03-24 12:44:10 -0400

    add gcd function

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,36 @@
+use serde::Deserialize;
 use actix_web::{web, App, HttpResponse, HttpServer};
+
+#[derive(Deserialize)]
+struct GcdParameters {
+    n: u64,
+    m:u64,
+}
 
 fn main() {
     let server = HttpServer::new(|| {
         App::new()
             .route("/", web::get().to(get_index))
+            .route("/gcd", web::post().to(post_gcd))
     });
 
     println!("Serving on http://localhost:3000...");
     server
         .bind("127.0.0.1:3000").expect("error binding server to address")
         .run().expect("error running server");
+}
+
+fn gcd(mut n:u64, mut m: u64) -> u64 {
+    assert!(n != 0 && m != 0);
+    while m != 0 {
+        if m < n {
+            let t =m;
+            m = n;
+            n = t;
+        }
+        m = m % n;
+    }
+    n
 }
 
 fn get_index() -> HttpResponse {
@@ -25,4 +46,21 @@ fn get_index() -> HttpResponse {
                 </form>
             "#,
         )
+}
+
+fn post_gcd(form: web::Form<GcdParameters>) -> HttpResponse {
+    if form.n ==0 || form.m ==0 {
+        return HttpResponse::BadRequest()
+            .content_type("text/html")
+            .body("Computing the GCD with zero is boring.");
+    }
+
+    let response =
+        format!("The greatest common divisor of the numbers {} and {} \
+                is <b>{}<b>\n",
+                form.n, form.m, gcd(form.n, form.m));
+
+    HttpResponse::Ok()
+        .content_type("text/html")
+        .body(response)
 }
